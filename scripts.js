dc72633f207d917850c53a7d17e6d52bff3fb9e3
SWEET16000 <sweet15nft@gmail.com>
2024-07-01 07:05:38 +0100

    Fixed icon hover effects and adjusted header link styles

--- scripts.js
+++ scripts.js
@@ -1,18 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const mobileMenu = document.getElementById('mobile-menu');
     const navList = document.querySelector('.nav-list');
-    const socialIcons = document.querySelectorAll('.social-icons img, .social-media img');
 
     mobileMenu.addEventListener('click', function() {
         navList.classList.toggle('nav-active');
-    });
-
-    socialIcons.forEach(icon => {
-        icon.addEventListener('mouseover', function() {
-            this.src = this.getAttribute('data-light');
-        });
-        icon.addEventListener('mouseout', function() {
-            this.src = this.getAttribute('data-dark');
-        });
     });
 });
